be1172ded604112ab32177f52c87a44b78886efd
metastasio <myaldzina.m@gmail.com>
2023-04-02 19:03:02 +0300

    Deleted src/index.copy; Modified src/index.js

--- src/index.js
+++ src/index.js
@@ -41,65 +41,11 @@ const gg = () => {
   console.log(`Congratulations, ${userName}!`);
 };
 
-// BRAIN EVEN
-const brainEven = () => {
-  let i = 0;
-
-  while (i < 3) {
-    const randomNumber = getRandomNumber();
-    questionText(randomNumber);
-    const answer = getAnswer();
-    const answerUnified = answer.toLowerCase();
-    const isEven = randomNumber % 2 === 0;
-
-    const correctAnswer = isEven ? 'yes' : 'no';
-
-    if (answerUnified === correctAnswer) {
-      console.log('Correct!');
-      i++;
-    } else {
-      console.log(
-        `'${answerUnified}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`,
-      );
-      i = 0;
-    }
-  }
-};
-
-// BRAIN CALC
-const brainCalc = () => {
-  let i = 0;
-
-  while (i < 3) {
-    const sign = getRandomSign();
-    const randomNumber = getRandomNumber();
-    const randomNumber2 = getRandomNumber();
-    const currentExpression = `${randomNumber} ${sign} ${randomNumber2}`;
-
-    questionText(currentExpression);
-    const userAnswer = getAnswer();
-    const userAnswerUnified = Number(userAnswer);
-    const expression = eval(currentExpression);
-
-    if (userAnswerUnified === expression) {
-      console.log('Correct!');
-      i++;
-    } else {
-      console.log(
-        `${userAnswer} is wrong answer ;(. Correct answer was ${expression}.\nLet's try again, ${userName}!`,
-      );
-      i = 0;
-    }
-  }
-};
-
 export {
   greetings,
   getUserName,
-  brainCalc,
   gg,
   getRandomSign,
-  brainEven,
   getAnswer,
   getRandomNumber,
   questionText,
